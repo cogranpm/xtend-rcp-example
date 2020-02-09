57c9014e3e71913845ea9c0229f4df670364a1fe
paul martin <mussym@live.com>
2020-02-09 10:36:21 -0600

    first commit

---
+++ rcp-kernai-test/src/com/parinherm/parts/xtend/XSamplePart.xtend
@@ -0,0 +1,71 @@
+package com.parinherm.parts.xtend
+
+import java.util.Arrays
+import java.util.List
+import javax.annotation.PostConstruct
+import javax.inject.Inject
+import org.eclipse.e4.ui.di.Focus
+import org.eclipse.e4.ui.di.Persist
+import org.eclipse.e4.ui.model.application.ui.basic.MPart
+import org.eclipse.jface.viewers.ArrayContentProvider
+import org.eclipse.jface.viewers.TableViewer
+import org.eclipse.swt.SWT
+import org.eclipse.swt.events.SelectionAdapter
+import org.eclipse.swt.events.SelectionEvent
+import org.eclipse.swt.layout.GridData
+import org.eclipse.swt.layout.GridLayout
+import org.eclipse.swt.widgets.Button
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Text
+
+class XSamplePart {
+
+	TableViewer tableViewer;
+
+	@Inject
+	MPart part;
+	
+	Text txtInput
+	Button btnTest
+
+	@PostConstruct
+	def void createComposite(Composite parent) {
+		parent.setLayout(new GridLayout(1, false));
+
+		btnTest = new Button(parent, SWT.PUSH)
+		btnTest.text = "Click"
+		btnTest.addSelectionListener(new SelectionAdapter {
+			
+			override widgetSelected(SelectionEvent e) {
+				txtInput.text = "nailed it bra"
+			}
+			
+		})
+
+		txtInput = new Text(parent, SWT.BORDER);
+		txtInput.setMessage("Hey, it's working")
+		txtInput.addModifyListener([e | part.setDirty(true)]);
+		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		tableViewer = new TableViewer(parent);
+
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		tableViewer.setInput(createInitialDataModel());
+		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+	}
+
+	@Focus
+	def void setFocus() {
+		tableViewer.getTable().setFocus();
+	}
+
+	@Persist
+	def void save() {
+		part.setDirty(false);
+	}
+	
+	def List<String> createInitialDataModel() {
+		return Arrays.asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5");
+	}
+	
+}
---
+++ rcp-kernai-test/xtend-gen/com/parinherm/parts/xtend/XSamplePart.java
@@ -0,0 +1,79 @@
+package com.parinherm.parts.xtend;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Text;
+
+@SuppressWarnings("all")
+public class XSamplePart {
+  private TableViewer tableViewer;
+  
+  @Inject
+  private MPart part;
+  
+  private Text txtInput;
+  
+  private Button btnTest;
+  
+  @PostConstruct
+  public void createComposite(final Composite parent) {
+    GridLayout _gridLayout = new GridLayout(1, false);
+    parent.setLayout(_gridLayout);
+    Button _button = new Button(parent, SWT.PUSH);
+    this.btnTest = _button;
+    this.btnTest.setText("Click");
+    this.btnTest.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(final SelectionEvent e) {
+        XSamplePart.this.txtInput.setText("nailed it bra");
+      }
+    });
+    Text _text = new Text(parent, SWT.BORDER);
+    this.txtInput = _text;
+    this.txtInput.setMessage("Hey, it\'s working");
+    final ModifyListener _function = (ModifyEvent e) -> {
+      this.part.setDirty(true);
+    };
+    this.txtInput.addModifyListener(_function);
+    GridData _gridData = new GridData(GridData.FILL_HORIZONTAL);
+    this.txtInput.setLayoutData(_gridData);
+    TableViewer _tableViewer = new TableViewer(parent);
+    this.tableViewer = _tableViewer;
+    this.tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+    this.tableViewer.setInput(this.createInitialDataModel());
+    Table _table = this.tableViewer.getTable();
+    GridData _gridData_1 = new GridData(GridData.FILL_BOTH);
+    _table.setLayoutData(_gridData_1);
+  }
+  
+  @Focus
+  public void setFocus() {
+    this.tableViewer.getTable().setFocus();
+  }
+  
+  @Persist
+  public void save() {
+    this.part.setDirty(false);
+  }
+  
+  public List<String> createInitialDataModel() {
+    return Arrays.<String>asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5");
+  }
+}
