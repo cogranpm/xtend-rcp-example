15781e5901df638660df34ed11d01f48f3c38125
Paul Martin <mussym@live.com>
2020-04-12 20:02:43 -0500

    testing out databinding
    it works great with the observable annotation

--- rcp-kernai-test/src/com/parinherm/parts/xtend/JFaceTest.xtend
+++ rcp-kernai-test/src/com/parinherm/parts/xtend/JFaceTest.xtend
@@ -1,15 +1,24 @@
 package com.parinherm.parts.xtend
 
+import com.parinherm.entity.xtend.Person
 import javax.annotation.PostConstruct
 import javax.inject.Inject
+import org.eclipse.core.databinding.Binding
+import org.eclipse.core.databinding.DataBindingContext
+import org.eclipse.core.databinding.beans.typed.BeanProperties
+import org.eclipse.core.databinding.observable.list.WritableList
+import org.eclipse.core.databinding.observable.value.WritableValue
 import org.eclipse.e4.ui.di.Focus
 import org.eclipse.e4.ui.di.Persist
 import org.eclipse.e4.ui.model.application.ui.basic.MPart
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties
+import org.eclipse.jface.databinding.viewers.ObservableListContentProvider
 import org.eclipse.swt.SWT
 import org.eclipse.swt.layout.GridData
 import org.eclipse.swt.widgets.Button
 import org.eclipse.swt.widgets.Composite
 import org.eclipse.swt.widgets.Text
+
 import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter
 
 class JFaceTest {
@@ -17,26 +26,57 @@ class JFaceTest {
 	@Inject
 	MPart part;
 	
-	Text txtInput
+	Text txtFirstName
+	Text txtLastName
 	Button btnTest
+	DataBindingContext ctx = new DataBindingContext()
+	WritableList<Person> input
+	WritableValue<Person> value = new WritableValue()
+	ObservableListContentProvider contentProvider = new ObservableListContentProvider()		
+	Person person = new Person()
+		
 		
 	@PostConstruct
 	def void createComposite(Composite parent) {
+ 		
+ 		person.firstName = "michael"
+ 		person.lastName = "tuck"
+ 		
  		btnTest = new Button(parent, SWT.PUSH)
 		btnTest.text = "Click"
 		
-		txtInput = new Text(parent, SWT.BORDER)
-		txtInput.message = "Hey, it's working"
+		txtFirstName = new Text(parent, SWT.BORDER)
+		txtLastName = new Text(parent, SWT.BORDER)
+
 		//lamba for this is cool
-		txtInput.addModifyListener([e | part.setDirty(true)])
-		txtInput.layoutData = new GridData(GridData.FILL_HORIZONTAL)
+
+		txtFirstName.layoutData = new GridData(GridData.FILL_HORIZONTAL)
+		txtLastName.layoutData = new GridData(GridData.FILL_HORIZONTAL)
+		
 
 		//lambda style event handler, thanks to static widgetSelectedAdapter method
 		btnTest.addSelectionListener(widgetSelectedAdapter([ e | 
-			txtInput.text = "lambda brah"
+			println(person.lastName + person.firstName)
 		]))
 		
 	
+		//data bindings
+		//move this to the list selection event
+		ctx.dispose()
+		val bindings = ctx.validationStatusProviders
+		bindings.forEach[element | 
+			ctx.removeBinding(element as Binding)
+		]
+		val firstNameOb = WidgetProperties.text(SWT.Modify).observe(txtFirstName)
+		val firstNameMod = BeanProperties.value("firstName").observeDetail(value)
+		val firstNameBinding = ctx.bindValue(firstNameOb, firstNameMod)
+
+		val lastNameOb = WidgetProperties.text(SWT.Modify).observe(txtLastName)
+		val lastNameMod = BeanProperties.value("lastName").observeDetail(value)
+		val lastNameBinding = ctx.bindValue(lastNameOb, lastNameMod)
+		
+		value.value = person
+		
  
  	}
  	
--- rcp-kernai-test/xtend-gen/com/parinherm/parts/xtend/JFaceTest.java
+++ rcp-kernai-test/xtend-gen/com/parinherm/parts/xtend/JFaceTest.java
@@ -1,48 +1,88 @@
 package com.parinherm.parts.xtend;
 
+import com.parinherm.entity.xtend.Person;
 import java.util.function.Consumer;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.ValidationStatusProvider;
+import org.eclipse.core.databinding.beans.typed.BeanProperties;
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
+import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.xtext.xbase.lib.InputOutput;
 
 @SuppressWarnings("all")
 public class JFaceTest {
   @Inject
   private MPart part;
   
-  private Text txtInput;
+  private Text txtFirstName;
+  
+  private Text txtLastName;
   
   private Button btnTest;
   
+  private DataBindingContext ctx = new DataBindingContext();
+  
+  private WritableList<Person> input;
+  
+  private WritableValue<Person> value = new WritableValue<Person>();
+  
+  private ObservableListContentProvider contentProvider = new ObservableListContentProvider<Object>();
+  
+  private Person person = new Person();
+  
   @PostConstruct
   public void createComposite(final Composite parent) {
+    this.person.setFirstName("michael");
+    this.person.setLastName("tuck");
     Button _button = new Button(parent, SWT.PUSH);
     this.btnTest = _button;
     this.btnTest.setText("Click");
     Text _text = new Text(parent, SWT.BORDER);
-    this.txtInput = _text;
-    this.txtInput.setMessage("Hey, it\'s working");
-    final ModifyListener _function = (ModifyEvent e) -> {
-      this.part.setDirty(true);
-    };
-    this.txtInput.addModifyListener(_function);
+    this.txtFirstName = _text;
+    Text _text_1 = new Text(parent, SWT.BORDER);
+    this.txtLastName = _text_1;
     GridData _gridData = new GridData(GridData.FILL_HORIZONTAL);
-    this.txtInput.setLayoutData(_gridData);
-    final Consumer<SelectionEvent> _function_1 = (SelectionEvent e) -> {
-      this.txtInput.setText("lambda brah");
+    this.txtFirstName.setLayoutData(_gridData);
+    GridData _gridData_1 = new GridData(GridData.FILL_HORIZONTAL);
+    this.txtLastName.setLayoutData(_gridData_1);
+    final Consumer<SelectionEvent> _function = (SelectionEvent e) -> {
+      String _lastName = this.person.getLastName();
+      String _firstName = this.person.getFirstName();
+      String _plus = (_lastName + _firstName);
+      InputOutput.<String>println(_plus);
     };
-    this.btnTest.addSelectionListener(SelectionListener.widgetSelectedAdapter(_function_1));
+    this.btnTest.addSelectionListener(SelectionListener.widgetSelectedAdapter(_function));
+    this.ctx.dispose();
+    final IObservableList<ValidationStatusProvider> bindings = this.ctx.getValidationStatusProviders();
+    final Consumer<ValidationStatusProvider> _function_1 = (ValidationStatusProvider element) -> {
+      this.ctx.removeBinding(((Binding) element));
+    };
+    bindings.forEach(_function_1);
+    final ISWTObservableValue<String> firstNameOb = WidgetProperties.<Text>text(SWT.Modify).observe(this.txtFirstName);
+    final IObservableValue<Object> firstNameMod = BeanProperties.<Person, Object>value("firstName").<Person>observeDetail(this.value);
+    final Binding firstNameBinding = this.ctx.<String, Object>bindValue(firstNameOb, firstNameMod);
+    final ISWTObservableValue<String> lastNameOb = WidgetProperties.<Text>text(SWT.Modify).observe(this.txtLastName);
+    final IObservableValue<Object> lastNameMod = BeanProperties.<Person, Object>value("lastName").<Person>observeDetail(this.value);
+    final Binding lastNameBinding = this.ctx.<String, Object>bindValue(lastNameOb, lastNameMod);
+    this.value.setValue(this.person);
   }
   
   @Focus
